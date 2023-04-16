5aaa8b7ef7ee1e2522b7496d09d41e6d7394bbe8
LM Yudhy Prayitno <119650090+Yudhy-code-stranger@users.noreply.github.com>
2023-04-16 23:42:13 +0800

    Inverse Matriks

--- Matriks-Invers.py
+++ Matriks-Invers.py
@@ -1,14 +1,94 @@
-# Sebelum menjalankan program, pastikan Anda telah menginstal numpy dengan menjalankan perintah "pip install numpy" di terminal atau command prompt
-import numpy as np
+def input_matrix():
+    n = int(input("Masukkan ukuran matriks: "))
+    if n > 6:
+        print("Maaf, ukuran matriks terlalu besar (maksimal 6x6).")
+        return None
+    else:
+        matrix = []
+        for i in range(n):
+            row = []
+            for j in range(n):
+                value = float(input("Masukkan nilai pada baris {} kolom {}: ".format(i+1, j+1)))
+                row.append(value)
+            matrix.append(row)
+        return matrix
 
-# Inisialisasi matriks A
-A = np.array([[1, 2], [3, 4]])
+def det(matrix):
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    elif n == 2:
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+    else:
+        result = 0
+        for j in range(n):
+            sign = (-1) ** j
+            sub_matrix = []
+            for i in range(1, n):
+                row = []
+                for k in range(n):
+                    if k != j:
+                        row.append(matrix[i][k])
+                sub_matrix.append(row)
+            sub_det = det(sub_matrix)
+            result += sign * matrix[0][j] * sub_det
+        return result
 
-# Menghitung invers matriks A
-A_inv = np.linalg.inv(A)
+def transpose(matrix):
+    n = len(matrix)
+    transposed = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            row.append(matrix[j][i])
+        transposed.append(row)
+    return transposed
 
-# Cetak matriks A dan invers matriks A
-print("Matriks A:")
-print(A)
-print("\nInvers matriks A:")
-print(A_inv)
+def cofactor(matrix):
+    n = len(matrix)
+    cofactored = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            sign = (-1) ** (i+j)
+            sub_matrix = []
+            for k in range(n):
+                if k != i:
+                    sub_row = []
+                    for l in range(n):
+                        if l != j:
+                            sub_row.append(matrix[k][l])
+                    sub_matrix.append(sub_row)
+            sub_det = det(sub_matrix)
+            row.append(sign * sub_det)
+        cofactored.append(row)
+    return cofactored
+
+def inverse(matrix):
+    determinant = det(matrix)
+    if determinant == 0:
+        print("Matriks tidak memiliki invers.")
+        return None
+    else:
+        n = len(matrix)
+        cofactors = cofactor(matrix)
+        adjugate = transpose(cofactors)
+        inverted = []
+        for i in range(n):
+            row = []
+            for j in range(n):
+                element = adjugate[i][j] / determinant
+                row.append(round(element, 2))
+            inverted.append(row)
+        return inverted
+
+matrix = input_matrix()
+if matrix is not None:
+    print("Matriks yang dicari inversnya:")
+    for row in matrix:
+        print(row)
+    inverse_matrix = inverse(matrix)
+    if inverse_matrix:
+        print("Invers matriks:")
+        for row in inverse_matrix:
+            print(row)
